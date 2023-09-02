e40043e96a683a0502bc29a997cb12fee92e4a5c
PACKOJIbHUKOB <jordn-5@yandex.ru>
2023-09-02 15:30:55 +0300

    задание выполнено

--- src/main/java/pro/sky/ListAndSets/EmployeeController.java
+++ src/main/java/pro/sky/ListAndSets/EmployeeController.java
@@ -1,15 +1,17 @@
 package pro.sky.ListAndSets;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping(path = "/employee")
 public class EmployeeController {
+
+    @ExceptionHandler(RuntimeException.class)
+    public String handlerException (RuntimeException e){
+        return e.getMessage();
+    }
     private final EmployeeService employeeService;
 
     public EmployeeController(EmployeeService employeeService) {
@@ -23,13 +25,13 @@ public class EmployeeController {
     @GetMapping(path = "/remove")
     public Employee remove(@RequestParam String firstName, @RequestParam String lastName){
         return employeeService.remove(firstName,lastName);
-
     }
+
     @GetMapping(path = "/find")
     public Employee find(@RequestParam String firstName, @RequestParam String lastName){
         return employeeService.find(firstName,lastName);
-
     }
+
     @GetMapping
     public List<Employee> getAll(){
         return employeeService.getAll();
--- src/main/java/pro/sky/ListAndSets/EmployeeService.java
+++ src/main/java/pro/sky/ListAndSets/EmployeeService.java
@@ -1,30 +1,47 @@
 package pro.sky.ListAndSets;
 
 import org.springframework.stereotype.Service;
+import pro.sky.ListAndSets.exceptions.EmployeeAlreadyAddedException;
+import pro.sky.ListAndSets.exceptions.EmployeeNotFoundException;
+import pro.sky.ListAndSets.exceptions.EmployeeStorageIsFullException;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class EmployeeService {
+    private static final int MaxSize =2;
+
    private final List<Employee> employees = new ArrayList<>();
 
     public Employee add(String firstName, String lastName) {
         Employee newEmployee = new Employee(firstName, lastName);
+        if (employees.contains(newEmployee)){
+            throw new EmployeeAlreadyAddedException("данный сотрудник уже существует");
+        }
+        if (employees.size()>MaxSize){
+            throw new EmployeeStorageIsFullException("Нет места для добавления сотрудника");
+        }
         employees.add(newEmployee);
         return newEmployee;
-
     }
+
     public Employee remove(String firstName, String lastName) {
         Employee removeEmployee = new Employee(firstName, lastName);
+        if (!employees.contains(removeEmployee)){
+            throw new EmployeeNotFoundException("невозможно удалить,данный сотрудник не найден");}
         employees.remove(removeEmployee);
         return removeEmployee;
     }
+
     public Employee find(String firstName, String lastName) {
         Employee findEmployee = new Employee(firstName, lastName);
         if (employees.contains(findEmployee)){
         return findEmployee;}
-        return null;
+        else {
+            throw new EmployeeNotFoundException("данный сотрудник не найден");
+        }
+
     }
     public List<Employee> getAll(){
         return employees;

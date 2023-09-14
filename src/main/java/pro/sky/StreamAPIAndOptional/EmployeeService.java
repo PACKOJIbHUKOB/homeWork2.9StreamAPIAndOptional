cee2a8728e9b809d3992c4079ed803a1cbbd5d4d
PACKOJIbHUKOB <jordn-5@yandex.ru>
2023-09-14 15:50:23 +0300

    Мастер ветка

---
+++ src/main/java/pro/sky/StreamAPIAndOptional/EmployeeService.java
@@ -0,0 +1,49 @@
+package pro.sky.StreamAPIAndOptional;
+
+import org.springframework.stereotype.Service;
+import pro.sky.StreamAPIAndOptional.exceptions.EmployeeAlreadyAddedException;
+import pro.sky.StreamAPIAndOptional.exceptions.EmployeeNotFoundException;
+import pro.sky.StreamAPIAndOptional.exceptions.EmployeeStorageIsFullException;
+
+import java.util.*;
+
+@Service
+public class EmployeeService {
+    private static final int MaxSize =2;
+
+    private final Map<String,Employee> employees = new HashMap<>();
+
+    public Employee put(String firstName, String lastName) {
+        Employee newEmployee = new Employee(firstName, lastName);
+        if (employees.containsKey(newEmployee.getKey())){
+            throw new EmployeeAlreadyAddedException("данный сотрудник уже существует");
+        }
+        if (employees.size()>MaxSize){
+            throw new EmployeeStorageIsFullException("Нет места для добавления сотрудника");
+        }
+        employees.put(newEmployee.getKey(), newEmployee);
+        return newEmployee;
+    }
+
+    public Employee remove(String firstName, String lastName) {
+        Employee removeEmployee = new Employee(firstName, lastName);
+        if (!employees.containsKey(removeEmployee.getKey())){
+            throw new EmployeeNotFoundException("невозможно удалить,данный сотрудник не найден");}
+        employees.remove(removeEmployee.getKey());
+        return removeEmployee;
+    }
+
+    public Employee find(String firstName, String lastName) {
+        Employee findEmployee = new Employee(firstName, lastName);
+        if (employees.containsKey(findEmployee.getKey())){
+            return findEmployee;}
+        else {
+            throw new EmployeeNotFoundException("данный сотрудник не найден");
+        }
+
+    }
+    public Collection<Employee> getAll(){
+        return Collections.unmodifiableCollection(employees.values());
+
+    }
+}
---
+++ src/main/java/pro/sky/StreamAPIAndOptional/StreamAPIAndOptionalApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.StreamAPIAndOptional;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamAPIAndOptionalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StreamAPIAndOptionalApplication.class, args);
+	}
+
+}
